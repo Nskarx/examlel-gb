af6a8f3a34838092231e6f629d46b150a94711b7
Evgeniy <Lymilight@gmail.com>
2022-11-18 19:20:28 +0700

    третье задание из дз

--- homeworkSemC#2/Program.cs
+++ homeworkSemC#2/Program.cs
@@ -3,24 +3,36 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 
-Console.Write("Введите трех значное число: ");
-int num =  Convert.ToInt32(Console.ReadLine());
-int a = num / 10;
-int c = num % 100;
-int ac = a%10;
-Console.WriteLine(ac);
+// Console.Write("Введите трех значное число: ");
+// int num =  Convert.ToInt32(Console.ReadLine());
+// int a = num / 10;
+// int c = num % 100;
+// int ac = a%10;
+// Console.WriteLine(ac);
 
-//
+
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-//Console.Write("Введите трех значное число: ");
+// Console.Write("Введите число: ");
+// int num =  Convert.ToInt32(Console.ReadLine());
+// int a = num / 10;
+// int c = num % 100;
+// int ac = c%10;
+// Console.WriteLine(ac);
 
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-
-
-
-
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// Console.Write("Введите число от 1 до 7:  ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num >= 1 && num <= 7)
+// {
+//     if (num == 6 | num == 7) Console.WriteLine("Сегодня выходной");
+//     else Console.WriteLine("Сегодня рабочий день");
+// }
 
