335ec1f2f2ed0803f7b6de719234a7d2da684a6f
Evgeniy <Lymilight@gmail.com>
2022-11-19 15:11:49 +0700

    какой-то пиздец

---
+++ lessonC#2/example011/Program.cs
@@ -0,0 +1,40 @@
+﻿int [] array = new int [10]; // означает создай новый массив размером 10
+void FillArray(int[] collection); // void - это метод который не возвращает не чего
+    int Length = collection.Length;
+    int index =0;
+{
+    while(index < Length)
+    {
+    collection[index] = new Random().Next(1, 10);
+    //index = index+1;
+    index++;
+    }
+}
+
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (positoin < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    index = 0;
+    while(index < count)
+    {
+        if(collection [index] == find)
+        
+    }
+}
+int[] array = new int [10];
+FillArray(array);
+PrintArray(array);
+
+
+
+
