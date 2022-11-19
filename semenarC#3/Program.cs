0f1f55b37109cf29b67b3c1eb6a91ac5aae17204
Evgeniy <Lymilight@gmail.com>
2022-11-19 19:59:22 +0700

    какой-то пиздец 3

--- semenarC#3/Program.cs
+++ semenarC#3/Program.cs
@@ -114,6 +114,24 @@
 // от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
+Console.WriteLine("Введите число до которого будет идти таблица");
+int N = Convert.ToInt32(Console.ReadLine());
+while (N == 0)
+{
+    Console.WriteLine("Неверный ввод, введите снова не нулевое число");
+    N = Convert.ToInt32(Console.ReadLine()); 
+}
+
+if (N>0)
+    for (int i = 1; i <= Math.Abs(N); i++)
+    {
+        Console.WriteLine($"Квадрат {i}: {Math.Pow(i, 2)}");
+    }
+else
+    for (int i = 1; i >= N; i--)
+    {
+        Console.WriteLine($"Квадрат {i}: {Math.Pow(i, 2)}");
+    }
 
 
 
